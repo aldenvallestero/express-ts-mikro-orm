e9177662c86aafad9120c6b61ada45d5b130c54d
Alden Vallestero <alden.v@agtuary.com>
2024-01-12 12:45:12 +0800

    ♻️ Make Server Startup Async

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
-import app from './configs/app-config';
+import http from 'http';
+import { Author, Book } from './entities';
+import express, { Express } from 'express';
 import { PORT } from './commons/app-references-common';
+import { EntityManager, EntityRepository, MikroORM } from '@mikro-orm/core';
 
-app.listen(PORT, () => console.log('Server started running on port', PORT));
+export const DI = {} as {
+  server: http.Server,
+  orm: MikroORM,
+  em: EntityManager,
+  bookRepository: EntityRepository<Book>,
+  authorRepository: EntityRepository<Author>,
+}
+
+export const app = (async () => {
+  DI.orm = await MikroORM.init();
+  DI.em = DI.orm.em;
+  DI.bookRepository = DI.orm.em.getRepository(Book);
+  DI.authorRepository = DI.orm.em.getRepository(Author);
+
+  const app: Express = express();
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.listen(PORT, () => console.log('Server started running on port', PORT));
+})();
