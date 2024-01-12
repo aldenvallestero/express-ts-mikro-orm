5df9dc98a68ae7ee9ff771a0b2a760950984cce3
Alden Vallestero <alden.v@agtuary.com>
2024-01-12 11:07:33 +0800

    🔧 Add App Types

---
+++ src/commons/app-types-common.ts
@@ -0,0 +1,7 @@
+export type T_DATABASE_CREDENTIALS = {
+  DB_HOST: string | undefined,
+  DB_NAME: string | undefined,
+  DB_USERNAME: string | undefined,
+  DB_PASSWORD: string | undefined,
+  DB_PORT: number | undefined,
+}
